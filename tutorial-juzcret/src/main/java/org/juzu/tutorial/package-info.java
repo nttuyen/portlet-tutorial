59fdd97837f2cdf5542a741d29f5b028fe2470d8
phuong_vu <phuong.vu@exoplatform.com>
2014-10-02 16:02:04 +0700

    Step 2 using @Binding for SecretService and add KernelProviderFactory

--- tutorial-juzcret/src/main/java/org/juzu/tutorial/package-info.java
+++ tutorial-juzcret/src/main/java/org/juzu/tutorial/package-info.java
@@ -15,4 +15,9 @@
  */
 
 @juzu.Application(defaultController = org.juzu.tutorial.JuZcretApplication.class)
+@Bindings({ @Binding(value = org.juzu.tutorial.services.SecretService.class, implementation = org.juzu.tutorial.services.SecretServiceMemImpl.class) })
 package org.juzu.tutorial;
+
+import juzu.plugin.binding.Binding;
+import juzu.plugin.binding.Bindings;
+
