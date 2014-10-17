55147788692db0fba54d7b3279cfab1e62cf8a1d
thibaultclem <tclement@exoplatform.com>
2014-10-17 17:22:55 +0700

    Enhance UI for like and comment feature

--- tutorial-juzcret/src/main/java/org/juzu/tutorial/assets/javascripts/secret.js
+++ tutorial-juzcret/src/main/java/org/juzu/tutorial/assets/javascripts/secret.js
@@ -1,42 +1,109 @@
-(function($) {
-  $(document).on('click.juzu.secret.addLike', '.btn-like', function() {
-    var jLike = $(this);
-    var jSecret = jLike.closest('.secret');
-    var secretId = jSecret.attr('data-secretId');
-    
-    jLike.jzAjax('JuZcretApplication.addLike()', {
-      data: {'secretId': secretId},
-      success: function(data) {
-        jSecret.find('.like-list').html(data);
-      }
-    });
-    return false;
-  });
-  
-  $(document).on('click.juzu.secret.addComment', '.btn-comment', function() {
-    var jComment = $(this);
-    var jSecret = jComment.closest('.secret');
-    var secretId = jSecret.attr('data-secretId');
-    
-    jComment.jzAjax('JuZcretApplication.addComment()', {
-      data: {'secretId': secretId, 'content': jComment.prev('input').val()},
-      success: function(data) {
-        if (typeof(data) == 'string') {
-          //error response
-          alert(data);
-        } else {
-          //update html
-          var cList = "";
-          $(data).each(function(idx, elem) {
-            if (elem.content) {
-              cList += "<li><div>" + elem.content + "</div></li>";             
-            }          
-          });
-          var html = jSecret.find('.comment-list').html();
-          jSecret.find('.comment-list').html(html + cList);          
+(function ($) {
+
+    $(document).ready(function () {
+
+        function getRangeRandom(min, max) {
+            return Math.ceil(Math.random() * (max - min) + min);
         }
-      }
+
+        function randSecretBoxWidth() {
+            var randBoxNum = getRangeRandom(23, 43);
+            //Test if we are on th third image of the line
+            if (counterImg >= 2) {
+                //The third image of the line fill all the remaining place
+                randBoxNum = 100 - totalWidthLine;
+                //counter place taken by previous image in the line set to 0
+                counterImg = 0;
+                totalWidthLine = 0;
+            }
+            else {
+                //Increase counter and the place taken by previous image in the line
+                counterImg++;
+                totalWidthLine += randBoxNum;
+            }
+            //Return the width of the secret box
+            return randBoxNum;
+        }
+
+        //Var to know the number of image in the line
+        var counterImg = 0;
+        //Var to know the place taken by previous image in the line
+        var totalWidthLine = 0;
+
+        //Get all secrets boxes
+        var nums = document.getElementsByClassName("secret-wall-list");
+        var listItem = nums[0].getElementsByTagName("li");
+
+        //Set a random width
+        for (var i = 0; i < listItem.length; i++) {
+            listItem[i].style.width = randSecretBoxWidth() + "%";
+        }
+
     });
-    return false;
-  });
+
+    $(document).on('click.juzu.secret.addLike', '.btn-like', function () {
+        var jLike = $(this);
+        var jSecret = jLike.closest('.secret');
+        var secretId = jSecret.attr('data-secretId');
+
+        jLike.jzAjax('JuZcretApplication.addLike()', {
+            data: {'secretId': secretId},
+            success: function (data) {
+                //jSecret.find('.like-list').html(data);
+                var jLikeIcon = jSecret.find('.btn-like');
+                jLikeIcon.find('.numb').text($(data).size());
+            }
+        });
+        return false;
+    });
+
+    $(document).on('click.juzu.secret.addComment', '.btn-popup-comment', function () {
+        var jComment = $(this);
+        var jSecret = jComment.closest('.secret');
+        jSecret.addClass('open-popover');
+    });
+
+    $(document).on('click.juzu.secret.addComment', '.btn-comment', function () {
+        var jComment = $(this);
+        var jSecret = jComment.closest('.secret');
+        var secretId = jSecret.attr('data-secretId');
+
+        jComment.jzAjax('JuZcretApplication.addComment()', {
+            data: {'secretId': secretId, 'content': jSecret.find('.secret-add-comment').val()},
+            success: function (data) {
+                if (typeof(data) == 'string') {
+                    //error response
+                    alert(data);
+                } else {
+                    //update html
+                    var cList = "";
+                    var cCounter = 0;
+                    $(data).each(function (idx, elem) {
+                        if (elem.content) {
+                            cList +=
+                                "<div class='media'>" +
+                                    "<a class='pull-left' href='http://localhost:8080/portal/intranet/profile/" + elem.userId + "'>" +
+                                        "<img src='https://pu3eia.blu.livefilestore.com/y2p_AtDs2zZybxjt4TnuAS8AM6_ig4OVyYdYf83xMvAm1ZO09VUMEX5cStpy2CHeeqE9XW3LdgEe_KPPdtKVcmYyBsBbmnRm2ngEt4n8DUMeE0/User_01.jpg' alt='avatar'>" +
+                                    "</a>" +
+                                    "<div class='media-body'>" +
+                                        "<div>" +
+                                            "<a class='cm-user-name' href='#'>" + elem.userId + "</a> " +
+                                            "<span class='cm-time'>" + elem.createdDate + "</span>" +
+                                        "</div>" +
+                                        "<div class='cm-content'>" + elem.content + "</div>" +
+                                    "</div>" +
+                                "</div>";
+                            cCounter++;
+                        }
+                    });
+                    var html = jSecret.find('.secr-comments-list').html();
+                    jSecret.find('.secr-comments-list').html(html + cList);
+                    var jCommentIcon = jSecret.find('.btn-popup-comment');
+                    var jCommentNumb = jCommentIcon.find('.numb').text();
+                    jCommentIcon.find('.numb').text(jCommentNumb+cCounter);
+                }
+            }
+        });
+        return false;
+    });
 })($);
--- tutorial-juzcret/src/main/java/org/juzu/tutorial/package-info.java
+++ tutorial-juzcret/src/main/java/org/juzu/tutorial/package-info.java
@@ -17,19 +17,21 @@
 @juzu.Application(defaultController = org.juzu.tutorial.JuZcretApplication.class)
 @Bindings({ @Binding(value = org.juzu.tutorial.services.SecretService.class, implementation = org.juzu.tutorial.services.SecretServiceMemImpl.class) })
 @WebJars(@WebJar("jquery"))
-@Scripts(
-   {
-       @Script(id = "jquery", value = "jquery/1.10.2/jquery.js"),
-       @Script(value = "javascripts/secret.js", depends = "jquery")
-     }
- )
+@Less(@Stylesheet("styles/juzcret.less"))
+@Scripts({
+    @Script(id = "jquery", value = "jquery/1.10.2/jquery.js"),
+    @Script(value = "javascripts/secret.js", depends = "jquery")
+})
 @Assets("*")
 package org.juzu.tutorial;
 
-import juzu.plugin.binding.Binding;
-import juzu.plugin.binding.Bindings;
 import juzu.plugin.asset.Assets;
 import juzu.plugin.asset.Script;
 import juzu.plugin.asset.Scripts;
+import juzu.plugin.asset.Stylesheet;
+import juzu.plugin.binding.Binding;
+import juzu.plugin.binding.Bindings;
+import juzu.plugin.less4j.Less;
 import juzu.plugin.webjars.WebJar;
 import juzu.plugin.webjars.WebJars;
+
